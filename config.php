be40e8ed0512ed5262050683d482a11f68329224
Yeager1711 <huynhnam1711102@gmaaail.com>
2022-02-17 10:17:04 +0700

    add file

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','shop_cart') or die('connection failed');
+
+?>
---
+++ header.php
@@ -0,0 +1,16 @@
+<header class="header">
+
+   <div class="flex">
+
+      <a href="#" class="logo">Thư mục</a>
+
+      <nav class="navbar">
+         <a href="admin.php">Admin</a>
+         <a href="products.php">Xem sản phẩm Update</a>
+      </nav>
+
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+   </div>
+
+</header>
